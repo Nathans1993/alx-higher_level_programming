a89c21f99e65812a5dea13c67526e6550b55d474
Nathaniel Joshua <natha113322@gmail.com>
2021-09-09 05:13:09 -0700

    fixing some errors

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,4 +5,4 @@ for a in range(0, 10):
             if a < 8:
                 print("{}{},".format(a, b), end="")
             else:
-                print("{}{}".format(a, b))
+                    print("{}{}".format(a, b))
