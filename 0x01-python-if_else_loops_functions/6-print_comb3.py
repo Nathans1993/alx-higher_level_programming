3c7ede7a09620802430f9e28c861fb76748a4950
Nathaniel Joshua <natha113322@gmail.com>
2021-09-09 05:06:46 -0700

    Program that prints all possible different combination of digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for a in range(0, 10):
+    for b in range(0, 10):
+        if i < j:
+            if i < 8:
+                print("{}{},".format(i, j), end="")
+            else:
+                print("{}{}".format(i, j))
